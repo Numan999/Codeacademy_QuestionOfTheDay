0b75b8e808abf5527cf495182c00ec90b15bc044
Numan999 <141605300+Numan999@users.noreply.github.com>
2023-08-07 19:23:29 +0400

    Add files via upload

---
+++ codeacademy/Numan/script.js
@@ -0,0 +1,46 @@
+/*
+var info = {
+    name:"numan",
+    age:21,
+    Nationality:"Omani"
+}
+
+console.log(info.name+ " "+info.age+" "+info.Nationality)
+*/
+
+/*
+var info = {
+    name:"Salim",
+    Age:22,
+    nationality:"Omani",
+    Number: Math.floor(Math.random()*100000000)
+
+
+
+
+}
+
+console.log(info)
+*/
+
+//  
+
+// if(info.Age===18){
+// console.log("perfect age");
+
+// }
+// else if(info.Age===22){
+// console.log("too old")
+// }
+// else{
+// console.log("Thank youu")
+// }
+
+var name = "moe";
+var age = 19;
+
+if (name === "moe" && age===19){
+    console.log("true")
+}else{
+console.log("false")
+}
