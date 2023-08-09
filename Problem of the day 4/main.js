8471d7a907625b3440e721f4ccee6e503d869a03
Numan999 <141605300+Numan999@users.noreply.github.com>
2023-08-10 00:53:08 +0400

    Add files via upload

---
+++ Problem of the day 4/main.js
@@ -0,0 +1,11 @@
+const userName = (name) =>{
+    const n = name;
+    if(n.length == 1){
+        console.log(`it's a boy`);
+    }else{
+        console.log(`it's a girl`)
+    }
+
+}
+
+userName("N")
