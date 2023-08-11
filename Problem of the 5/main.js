b0655b6a4861967b482e65e498739c17ee181bf1
Numan999 <141605300+Numan999@users.noreply.github.com>
2023-08-12 02:06:29 +0400

    Add files via upload

---
+++ Problem of the 5/main.js
@@ -0,0 +1,12 @@
+const arr = [1,2,2,8];
+const arrr = [2,4,6,12];
+count = 0;
+for(i=0;i<=3;i++){
+    if( (arr[i] / arrr[i] + arr[i] / arrr[i]) === 1){
+      count+=1
+      
+    }
+    }
+    console.log(`there are ${count} number of pairs`)
+
+
