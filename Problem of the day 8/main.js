5772b76ce0fd8fae1ba919730b28c1291fd20e18
Numan999 <141605300+Numan999@users.noreply.github.com>
2023-08-19 19:43:22 +0400

    Update main.js

--- Problem of the day 8/main.js
+++ Problem of the day 8/main.js
@@ -1,11 +1,13 @@
-const no = Number(prompt("Enter numbers of elements in the array: "))
-
 const array=[];
+const a =(n)=>{
 
-for ( let i = 0 ; i < no ; i++){
+for ( let i = 0 ; i < n ; i++){
     const element = prompt("Enter item "+ (i+1) + " :");
     array.push(element)
 }
 const reverse = array.reverse();
 
 console.log(reverse)
+}
+
+a(3)
