062a0885e7d66de1be24406c717ca88062844eca
Numan999 <141605300+Numan999@users.noreply.github.com>
2023-08-28 13:00:42 +0400

    Add files via upload

---
+++ Problem of the day 11/main.js
@@ -0,0 +1,26 @@
+function alternative(arr) {
+    const posNums = arr.filter(num => num > 0);
+    const negNums = arr.filter(num => num < 0);
+
+    const result = [];
+    let posIdx = 0, negIdx = 0;
+
+    for (const num of arr) {
+        if (num > 0) {
+            result.push(posNums[posIdx]);
+            posIdx++;
+        } else {
+            result.push(negNums[negIdx]);
+            negIdx++;
+        }
+    }
+
+    return result;
+}
+
+ 
+
+//The input 
+const Arr = [9, 4, -2, -1, 5, 0, -5, -3, 2];
+const result = alternative(Arr);
+console.log(result);
